4cc4d77f7ebe5976a6f973b428742b399430f999
Léo Delpon <45091304+LeoDELPON@users.noreply.github.com>
2019-11-13 13:11:05 +0100

    Resetup new node server

---
+++ index.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="theme-color" content="#000000" />
+    <meta name="description" content="Cinq élèves de CESI Bordeaux on voulu réaspanser un site pour leur BDE dans le cadre d'un projet.">
+    <meta http-equiv="pragma" content="no-cache" />
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
+    <link rel="stylesheet" href="src/css/index.css" />
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+
+
+    <link rel="icon" type="image/png" href="src/img/cesiLogo32.png" sizes="32x32" />
+    <link rel="icon" type="image/png" href="src/img/cesiLogo64.png" sizes="64x64" />
+    <link rel="apple-touch-icon" href="src/img/cesiLog32.png" sizes="32x32" />
+    <link rel="apple-touch-icon" href="src/img/cesiLogo64.png" sizes="64x64" />
+    <title>BDE CESI</title>
+</head>
+
+<body>
+    <div class="container-fluid header">
+        <div class="row">
+            <div class="col-md-4 pos-logo">
+                <img src="src/img/headerImage/cesiLogo.svg" alt="image du logo de cesi" />
+            </div>
+            <div class="col-md-4 pos-nav-menu">
+                <span><a href="#">ECOLE</a></span>
+                <span><a href="#">EQUIPE</a></span>
+                <span><a href="#">CLUB&ASSOC</a></span>
+                <span><a href="#">BOUTIQUE</a></span>
+                <span><a href="#">EVENT</a></span>
+
+            </div>
+            <div class="col-md-4 pos-icon">
+                <a href="#"><button type="button" class="btn btn-outline-dark">Se connecter</button></a>
+                <a href="#"><img src="src/img/headerImage/searchIcon.png" alt="icon de la barre de recherche" /></a>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid">
+        <img class="image-size" src="src/img/headerImage/bannerImage.jpg" alt="image de fond du home page du BDE de Cesi" />
+        <div class="inner-banner">
+            <div>
+                <span>
+                    SITE DU BUREAU DES ELEVES
+                </span>
+            </div>
+            <div>
+                <span>
+                    CESI INGENIEUR
+                </span>
+            </div>
+            <div>
+                <button type="button" class="btn btn-outline-warning">CONTACTEZ-NOUS</button>
+            </div>
+        </div>
+    </div>
+    <section class="container-fluid">
+        <div class="space">
+        </div>
+    </section>
+    <section>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-5">
+                    <div class="school-background">
+                    </div>
+                </div>
+                <div class="col-md-7">
+                    <div class="displaying-school-text">
+                        Le CESI est une école d'ingénieur certifié par le CTI. Sa différence majeur vis-à-vis des autres écoles est
+                        a modularité du cursus qu'on peut y suivre et la méthode d'apprentissage. En effet, au fur et a mesure de leur scolarité
+                        es eleves, en fonction de leur envie futur vont pouvoir choisir des spécialisations. Ce choix d'enseignement permet aux
+                        eleves d'etre parfaitement adapté au domaine qu'il visent et de limiter l'apprentissage de domaine inutile proffessionellement
+                        parlant. Enfin la methode PBL est une méthode d'apprentissage consistant à mettre les eleves en face de probleme et de stimuler
+                        leur envie de rechercher les solutions a ce probleme dans le but de leur apprendre a apprendre par eux meme. En effet cette
+                        competence est trés utiles en entreprise dans des domaines aussi dynamique que l'informatique.
+                    </div>
+                </div>
+                <div class="container-fluid momo">
+                    <div class="displaying-title">
+                        <span>
+                            NOTRE ECOLE
+                        </span>
+                    </div>
+                    <div class="displaying-image">
+                        <img src="src/img/schoolSection/cesiLogo.png" class="resize-logo-image" alt="image du logo de CESI" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="container-fluid">
+        <div class="space">
+        </div>
+    </section>
+    <section>
+        <div class="container-fluid">
+            <div class="row">
+                <div class=" col-md-8 pos-whoAreWe">
+                    <div class="title-whoAreWe">
+                        <span>
+                            QUI SOMMES-NOUS?
+                        </span>
+                    </div>
+                    <div class="displaying-whoAreWe">
+                        Le CESI est une école d'ingénieur certifié par le CTI. Sa différence majeur vis-à-vis des autres écoles est
+                        a modularité du cursus qu'on peut y suivre et la méthode d'apprentissage. En effet, au fur et a mesure de leur scolarité
+                        es eleves, en fonction de leur envie futur vont pouvoir choisir des spécialisations. Ce choix d'enseignement permet aux
+                        eleves d'etre parfaitement adapté au domaine qu'il visent et de limiter l'apprentissage de domaine inutile proffessionellement
+                        parlant. Enfin la methode PBL est une méthode d'apprentissage consistant à mettre les eleves en face de probleme et de stimuler
+                        leur envie de rechercher les solutions a ce probleme dans le but de leur apprendre a apprendre par eux meme. En effet cette
+                        competence est trés utiles en entreprise dans des domaines aussi dynamique que l'informatique.
+                    </div>
+                </div>
+                <div class="col-md-4 displaying-whoAreWeImage">
+                    <img class="resize-whoAreWeImage" src="src/img/whoAreWe/fraternity.png" alt="image de notre fraternité française ou BDE" />
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="container-fluid">
+        <div class="space">
+        </div>
+    </section>
+    <section class="container-fluid displaying-dashBoard">
+        <div class="container dashboard">
+            <div class="row">
+                <div class="col-md-3 orange-box">
+                    <div>
+                        <span>
+                            5
+                        </span>
+                    </div>
+                    <div>
+                        <span>
+                            Associations
+                        </span>
+                    </div>
+                </div>
+                <div class="col-md-3 white-box">
+                    <div>
+                        <span>
+                            10
+                        </span>
+                    </div>
+                    <div>
+                        <span>
+                            Membres du BDE
+                        </span>
+                    </div>
+                </div>
+                <div class="col-md-6 title-dashBoard">
+                    <div>
+                        <span>
+                            LE BDE EN QUELQUES CHIFFRES
+                        </span>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-3 white-box">
+                    <div>
+                        <span>
+                            12
+                        </span>
+                    </div>
+                    <div>
+                        <span>
+                            Evenements organisés
+                        </span>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                </div>
+                <div class="col-md-3 orange-box">
+                    <div>
+                        <span>
+                            116
+                        </span>
+                    </div>
+                    <div>
+                        <span>
+                            Inscrits
+                        </span>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                </div>
+            </div>
+    </section>
+</body>
+
+</html>
