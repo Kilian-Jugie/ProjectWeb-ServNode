c85782cc25cd1bbed77d9460274f289bea9438c5
Léo Delpon <45091304+LeoDELPON@users.noreply.github.com>
2019-11-16 13:14:08 +0100

    Fixed some front-bug caused by concatenation of the back-end dev

--- navBar.php
+++ navBar.php
@@ -15,14 +15,15 @@
         //session_start();
         if (isset($_SESSION['user_log'])) {
             echo "
-                    <div class='col-md-4 pos-icon-shop'>
-                        <span>Bonjour " . $_SESSION['user_log']->pseudo . "<span>
+                    <div class='col-md-4 pos-icon'>
+                        <a href=''><span class='pseudo'>Bonjour " . $_SESSION['user_log']->pseudo . "<span></a>
                         <a href='{$pathForLog}Log_outScript.php'><button type='button' class='btn btn-outline-dark'> Se deconnecter</button></a>
                         <a href='#'><img src={$pathImg}src/img/headerImage/searchIcon.png' alt='icon de la barre de recherche' /></a>
                     </div>
                 ";
         } else {
             echo "<div class='col-md-4 pos-icon'>
+            <a href=''><span>Bonjour suceurDeQueue</span></a>
             <a href=' {$pathForLog}login.php'><button type='button' class='btn btn-outline-dark'>Se connecter</button></a>
             <a href='#'><img src='{$pathImg}src/img/headerImage/searchIcon.png' alt='icon de la barre de recherche' /></a>
         </div>";
--- shop/shopNavBar.php
+++ shop/shopNavBar.php
@@ -16,7 +16,7 @@
         if (isset($_SESSION['user_log'])) {
             echo "  
                     <div class='col-md-4 pos-icon-shop'>
-                        <span>Bonjour " . $_SESSION['user_log']->pseudo . "<span>
+                        <a href=''><span>Bonjour " . $_SESSION['user_log']->pseudo . "<span></a>
                         <a href='cart.php'><img src='../src/img/shop/shopping-cart.svg' /></a>
                         <a href='../index/login_system/Log_outScript.php'><button type='button' class='btn btn-outline-dark'> Se deconnecter</button></a>
                         <a href='#'><img src=../src/img/headerImage/searchIcon.png' alt='icon de la barre de recherche' /></a>
@@ -24,6 +24,7 @@
                 ";
         } else {
             echo "<div class='col-md-4 pos-icon-shop'>
+            <a href=''><span>Bonjour suceurDeQueue</span></a>
             <a href='cart.php'><img src='../src/img/shop/shopping-cart.svg' /></a>
             <a href='../index/login_system/login.php'><button type='button' class='btn btn-outline-warning'>Se connecter</button></a>
             <a href='#'><img src='../src/img/headerImage/searchIonWhite.png' alt='icon de la barre de recherche' /></a>
