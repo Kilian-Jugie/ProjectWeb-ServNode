cfaf393e989c051920ceb6af703aaea250893126
BlandinRemi <33541014+BlandinRemi@users.noreply.github.com>
2019-11-18 01:14:19 +0100

    tweaks et nouvelle mixin

--- index/event.php
+++ index/event.php
@@ -33,15 +33,19 @@
     <div class="container-fluid displaying-event">
         <h1>LES EVENTS</h1>
         <div class="row content">
-            <div class="col-md-7  ImageAndDate">
+            <div class="col-md-6  ImageAndDate">
                 <img src="../src/img/associationImg/ballIsLifeImage.png" alt="imgContact" class="associationImg">
                 <p>Date: 10/4/2020</p>
             </div>
 
-            <div class="col-md-5 associationText">
+            <div class="col-md-6 associationText">
                 <h2>BALL IS LIFE</h2>
                 <p>Hey ! C'est Aurélien, le responsable de l'association de basketball ! On fait principalement du basket mais on s'amuse aussi à jouer du pied de temps en temps. On y va en début d'après-midi le jeudi pendant la vie asso. On va au complexe sportif près du stade MATMUT-ATLANTIQUE. Donc si tu aime te détendre en courant après un ballon n'hésite pas a nous rejoindre. Pas de compétition pas de jugement juste du fun !</p>
-                <div><a href="#">Accéder à l'association</a></div>
+                <div>
+                    <button class="reactionButton">Like</button>
+                    <button class="reactionButton">Dislike</button>
+                    <a href="association.php">Accéder à l'association</a>
+                </div>
             </div>
         </div>
 
@@ -54,7 +58,7 @@
             <div class="col-md-5 associationText">
                 <h2>PROGRAMMING</h2>
                 <p>Bonjour ! C'est Julie, la responsable de l'association de programmation ! On y fait des tonnes de mini-projet dans pleins de langages différents. Ca se déroule de 13h30 à 17h tout les jeudis. Tout ceci se déroule en salle 104 au premier étage du batiment. Enfin bref ! Si tu kiffe faire du développement que ce soit sous Java, Angular ou même cobol ramène-toi ! on pourra même faire des GameJam !</p>
-                <div><a href="#">Accéder à l'association</a></div>
+                <div><a href="association.php">Accéder à l'association</a></div>
             </div>
         </div>
 
@@ -67,7 +71,7 @@
             <div class="col-md-5 associationText">
                 <h2>R00T-M3</h2>
                 <p>H3110 ! C'est Eliott, le responsable de l'association R00T-M3 ! Ici on hack toutes sorte de trucs, du serveur au raspberry Py en passant par le graal des sites de hackers "R00T-M3". On s'y met dés 13h30 et on brute-force à tour de bras jusqu'a 17h ! Rendez-vous en salle 105 le jeudi si tu as envie de te la jouer F_Society. Si tu as lu jusqu'ici c'est que tu dois être féru de cybersécurité alors viens donc DDos quelque serveurs avec nous. A l'occasion on se fera même un ctf ou deux!</p>
-                <div><a href="#">Accéder à l'association</a></div>
+                <div><a href="association.php">Accéder à l'association</a></div>
             </div>
         </div>
 
@@ -80,7 +84,7 @@
             <div class="col-md-5 associationText">
                 <h2>ELOQUENCE</h2>
                 <p>Salut ! C'est Benjamin, le responsable de l'association eloquence ! Dans notre association on apprend à discuter, débattre, prendre confiance en soi, tout ca dans le calme et la bonne humeur ! On Commence habituellement vers 14h salle 108 tout les jeudis ou presque, alors soit au rendez-vous pour apprendre à parler en usant de ton charisme, ta confiance et ton vocabulaire. Donc si tu préfere le calme et l'enrichissement de l'esprit c'est chez nous que ca se passe. On organise aussi des débats d'éloquences</p>
-                <div><a href="#">Accéder à l'association</a></div>
+                <div><a href="association.php">Accéder à l'association</a></div>
             </div>
         </div>
 
@@ -93,7 +97,7 @@
             <div class="col-md-5 associationText">
                 <h2>GAMING</h2>
                 <p>Hej ! C'est Aart, le responsable de l'association de gaming ! Avec nous c'est de la détente et des délires. On se met tous devant un écran et on fait plein de jeux de combats ou coop ensemble tout en se marrant (et en rageant aussi). On se réuni en salle 109 chaque jeudis à partir de 13h30 pour geeker un coup et décompresser. On fait aussi des jeux de rôles, libre à toi d'en apporter pour qu'on y joue ensemble, c'est sur qu'on va s'éclater. Du coup, si tu cherches un mates pour faire des games un jeudi aprem viens chez nous !</p>
-                <div><a href="#">Accéder à l'association</a></div>
+                <div><a href="association.php">Accéder à l'association</a></div>
             </div>
         </div>
     </div>
